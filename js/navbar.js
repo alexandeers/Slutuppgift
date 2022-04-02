d5b62f318c5cfe59f11a2145ab9016b8fc10ae99
Alexander Sandberg <alexander.sandberg78@gmail.com>
2022-04-02 03:49:06 +0200

    2.1

--- js/navbar.js
+++ js/navbar.js
@@ -2,6 +2,7 @@ function ToggleNavbar() {
     $(".segment-1").toggleClass("segment-1-closed");
     $(".segment-2").toggleClass("segment-2-closed");
     $(".segment-3").toggleClass("segment-3-closed");
+    $(".hamburger-button").toggleClass("hamburger-button-closed");
 
     $(".nav-background").toggleClass("nav-background-toggle");
     $(".nav-background").toggleClass("nav-extend-background");
@@ -10,7 +11,19 @@ function ToggleNavbar() {
 window.addEventListener('scroll', function() {
     if(this.window.scrollY > 50) {
         $("nav").addClass("decrease-text-after-scroll");
+        $(".nav-text").addClass("black-text");
+        $(".selected").addClass("black-text");
     } else {
+        $(".nav-text").removeClass("black-text");
+        $(".selected").removeClass("black-text");
         $("nav").removeClass("decrease-text-after-scroll")
     }
+});
+
+
+$(document).ready(function () {
+    var image = document.getElementsByClassName('background');
+    new simpleParallax(image, {
+        scale: 0.9 
+    });
 });
