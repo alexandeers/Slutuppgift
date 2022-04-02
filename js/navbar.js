f75ca91480d028a1a93cd31cce81c2cec26f63c0
Alexander Sandberg <alexander.sandberg78@gmail.com>
2022-04-02 21:47:49 +0200

    3.0
    Fullständigt responsiv till i stort sett alla enheter. Vissa funktioner och styling-parametrar kommer inte att komma till full verkan i vissa enheter beroende på hur uppdaterade enheterna är. Jag har satsat på att använda så simpla CSS-parametrar som möjligt för att göra det så pass tillgängligt för alla enheter som möjligt. Såklart så struntar jag i support för Internet Explorer, och äldre enheter som gamla 3DS och TV-apparater och deras browser-support, eftersom det är helt enkelt inte rimligt att någon skulle besöka den här hemsidan på en sådan enhet. Hemsidan kommer fungera galant på moderna enheter, kanske inte lika bra på äldre men all information kommer fortfarande att vara tillgänglig oavsett enhet.

--- js/navbar.js
+++ js/navbar.js
@@ -4,16 +4,17 @@ function ToggleNavbar() {
     $(".segment-3").toggleClass("segment-3-closed");
     $(".hamburger-button").toggleClass("hamburger-button-closed");
 
-    $(".nav-background").toggleClass("nav-background-toggle");
     $(".nav-background").toggleClass("nav-extend-background");
 }
 
 window.addEventListener('scroll', function() {
     if(this.window.scrollY > 50) {
+        $(".hamburger-button").addClass("hamburger-button-black");
         $("nav").addClass("decrease-text-after-scroll");
         $(".nav-text").addClass("black-text");
         $(".selected").addClass("black-text");
     } else {
+        $(".hamburger-button").removeClass("hamburger-button-black");
         $(".nav-text").removeClass("black-text");
         $(".selected").removeClass("black-text");
         $("nav").removeClass("decrease-text-after-scroll")
