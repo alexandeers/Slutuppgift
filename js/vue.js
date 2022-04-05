8f32f6b209ee285b3ac5a94cdcbe62d45214d815
Alexander Sandberg <alexander.sandberg78@gmail.com>
2022-04-05 22:44:10 +0200

    3.6
    Lade till animationer för menyn via GSAP

--- js/vue.js
+++ js/vue.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import menujson from "./menu.json" assert { type: "json" };
+// import { gsap } from '../gsap-public/minified/gsap.min.js'
+// import { gsap } from 'gsap'
 
 // fetch("js/menu.json")  
 // .then(response => response.json())
@@ -8,15 +10,28 @@ import menujson from "./menu.json" assert { type: "json" };
 // );
       
 createApp({
-    data() {
-        return {
-          menu: menujson,
-          category: 0
-        }
-      },
-    methods: {
-        ChangeCategory() {
-            category = 2;
-        }
-    }
+	data() {
+		return {
+			menu: menujson,
+			category: 0,
+			timerCount: 10,
+			animationTrigger: false
+		}
+	},
+
+	methods: {
+		beforeEnter(el) {
+			el.style.opacity = 0
+			el.style.transform = "translateY(30px)"
+		},
+
+		enter(el) {
+			gsap.to(el, {
+				opacity: 1,
+				y: 0,
+				duration: 1,
+				delay: el.dataset.index*0.1
+			});
+		}
+	}
 }).mount('#menuApp');
